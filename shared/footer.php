d25025f78dc8407f28ec3db43f68c325c73be52b
Sergey Bobrov <s.bobrov.mail@gmail.com>
2012-11-24 12:52:38 -0500

    Experiment with popup window

--- shared/footer.php
+++ shared/footer.php
@@ -70,12 +70,14 @@
 
           var form_info_city = document.getElementById("geo-city").innerHTML;
           var form_info_status = document.getElementById("code-info").innerHTML;
-          var citylist = frameDoc.forms["checkcodeform"]["citylist"];
+          var citylist = frameDoc.getElementById("citylist");
           var city = form_info_city.replace(", MA", "");
 
           frameDoc.getElementById("geo-city").innerHTML = city;
           frameDoc.getElementById("code-info").innerHTML = form_info_status;
           citylist.value = city;
+
+          alert(citylist.length);
           
     			//Fade in the Popup and add close button
     			$('#' + popID).fadeIn().css({ 'width': Number( popWidth ) }).prepend('<a href="#" class="close"><img src="/images/close_button.png" class="btn_close" title="Close Window" alt="Close" /></a>');
