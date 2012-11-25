743abf8a63b19a459dfb68f80a17ac289e532f75
Sergey Bobrov <s.bobrov.mail@gmail.com>
2012-11-24 19:45:59 -0500

    Experiment with popup window

--- jquery/check_code_form.php
+++ jquery/check_code_form.php
@@ -117,7 +117,7 @@
 
 		function closeWindow(){
 			parent.$('a.close, #fade').click();
-			parent.$('a.poplight, #contact-link').click();
+			parent.$('a#contact-link').click();
 			
 			return false;
 		}
